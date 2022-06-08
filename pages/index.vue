bb7760fa9aa71de5be9c4a6731323301ec62f229
Lourdes <lourdesmarco.design@gmail.com>
2022-06-08 13:21:31 +0200

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- render the audio player -->
+  <audio-player />
+</template>
+
+<script>
+import AudioPlayer from '../components/AudioPlayer.vue'
+export default {
+  components: { AudioPlayer },
+  name: 'IndexPage'
+}
+</script>
+
+
+<style lang="postcss">
+
+</style>
